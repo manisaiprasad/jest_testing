760f553ade78404962b9c8cb0f45a154872e7c57
manisaiprasadam <manisaiprasadam@gmail.com>
2022-02-24 17:02:32 +0530

    jest tests added for dugeon

---
+++ index.js
@@ -0,0 +1,16 @@
+export function sum(a, b) {
+  return a + b;
+}
+
+export function dugeon(hero) {
+  const { strength, iq } = hero;
+  if (strength >= 50 && iq >= 90) {
+    return `${hero.hero}⚔️💰💍👑`;
+  } else if (strength >= 50) {
+    return `${hero.hero}⚔️🏥`;
+  } else if (iq >= 90) {
+    return `${hero.hero}🐍💰💍👑`;
+  } else {
+    return `💀`;
+  }
+}
